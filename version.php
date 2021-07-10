de4f3e15604c67ce147fd9c75c61da7393ac9e5e
eWallah <info@eWallah.net>
2021-07-11 00:00:08 +0200

    3.185.10

--- version.php
+++ version.php
@@ -27,7 +27,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2021030100;
-$plugin->release   = "3.185.6";
+$plugin->release   = "3.185.10";
 $plugin->requires  = 2013111811;
 $plugin->component = 'local_aws';
 $plugin->maturity  = MATURITY_STABLE;
